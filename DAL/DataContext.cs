69f497a58b51a223d44c255023303768d2b45f15
Hòa Nguyễn <shin.l0v3.ly@gmail.com>
2020-08-24 11:51:30 +0700

    no message

--- DAL/DataContext.cs
+++ DAL/DataContext.cs
@@ -12,6 +12,7 @@ namespace DTO
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+<<<<<<< HEAD
             // var builder = new ConfigurationBuilder()
             //    .SetBasePath(Directory.GetCurrentDirectory())
             //    .AddJsonFile("appsettings.json");
@@ -19,6 +20,10 @@ namespace DTO
             //optionsBuilder.UseSqlServer(configuration["ConnectionStrings:DefaultConnection"]);
             optionsBuilder.UseSqlServer(@"Server=HOANGUYEN;Database=QLXeDuLich;UID=sa;Password=Ta0lacuamay1;");
             //optionsBuilder.UseSqlServer(@"Server=HOANGUYEN;Database=QLXeDuLich;trusted_user=true");
+=======
+            optionsBuilder.UseSqlServer(@"Server=HOANGUYEN;Database=QLXeDuLich;trusted_user=true");//Đổi tên server,database thì điền tên database vừa tạo
+            //optionsBuilder.UseSqlServer(@"Server=HOANGUYEN;Database=QLXeDuLich;UID=sa;Password=Ta0lacuamay1;");
+>>>>>>> b1c07953366e899bd5298fefe2ac4a6858cd3142
         }
 
         //khai bao su dung class o day
